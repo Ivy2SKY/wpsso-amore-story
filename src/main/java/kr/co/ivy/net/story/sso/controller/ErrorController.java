1db2c46a6e3f9c5bc36cbf621cbb765b4e781b6e
bumsam <bumsam@linkproject.global>
2024-04-08 16:51:04 +0900

    test 커밋

--- src/main/java/kr/co/ivy/net/story/sso/controller/ErrorController.java
+++ src/main/java/kr/co/ivy/net/story/sso/controller/ErrorController.java
@@ -1,33 +1,33 @@
-package kr.co.ivy.net.story.sso.controller;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashMap;
-
-@Slf4j
-@Controller
-public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-
-
-    @RequestMapping(value = "/error")
-    public ResponseEntity<HashMap<String, Object>> errorRes() {
-
-        HashMap<String, Object> rtn = new HashMap<>();
-
-        try {
-
-            rtn.put("status", 500);
-
-        } catch (Exception e) {
-            log.error("" + e);
-        }
-
-        return new ResponseEntity<>(rtn, new HttpHeaders(), HttpStatus.OK);
-    }
-
-}
+//package kr.co.ivy.net.story.sso.controller;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import java.util.HashMap;
+//
+//@Slf4j
+//@Controller
+//public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+//
+//
+//    @RequestMapping(value = "/error")
+//    public ResponseEntity<HashMap<String, Object>> errorRes() {
+//
+//        HashMap<String, Object> rtn = new HashMap<>();
+//
+//        try {
+//
+//            rtn.put("status", 500);
+//
+//        } catch (Exception e) {
+//            log.error("" + e);
+//        }
+//
+//        return new ResponseEntity<>(rtn, new HttpHeaders(), HttpStatus.OK);
+//    }
+//
+//}
